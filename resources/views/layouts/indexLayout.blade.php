3a22a077584eda05a1bc4c5ce3d02aab9f92455e
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-05-25 17:28:23 +0200

    Create indexLayout.blade.php

---
+++ resources/views/layouts/indexLayout.blade.php
@@ -0,0 +1,12 @@
+@include("../partials/head")
+@include("../partials/header")
+@include("../partials/main")
+@include("../partials/banner")
+@include("../partials/footer")
+<body>
+    <div class="container">
+        @yield("content")
+    </div>
+
+</body>
+</html>
