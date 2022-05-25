f3926a8d593e3bd3f93b853c5507bd545b9cd7f5
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-05-25 17:28:12 +0200

    Update index.blade.php

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,1 +1,16 @@
-@dump($comics)
+@extends("../layouts/indexLayout")
+
+@section("content")
+ @foreach ($comics as $comic)
+    <div class="card">
+        <div class="card-header">
+            <h3>{{ $comic->title }}</h3>
+        </div>
+        <div class="card-body">
+            <p>{{ $comic->description }}</p>
+        </div>
+    </div>
+
+ @endforeach
+
+@endsection
