911df71d4f11172fbc7e4667b8b71ddf7da94c12
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-05-25 22:40:32 +0200

    Update show.blade.php

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,1 +1,45 @@
-@dump($comic)
+@extends('layouts.showLayout')
+
+@section('title')
+    {{$comic['title']}}
+@endsection
+{{--------------------------------------------------------------------------}}
+
+@section('series')
+    {{$comic['series']}}
+@endsection
+{{--------------------------------------------------------------------------}}
+
+@section('price')
+    {{$comic['price']}}
+@endsection
+{{--------------------------------------------------------------------------}}
+
+@section('description')
+    {{$comic['description']}}
+@endsection
+{{--------------------------------------------------------------------------}}
+
+{{-- @section('artists')
+    @foreach ($comic['artists'] as $artist)
+        <span class="artist">{{ $artist }}</span>,
+    @endforeach
+@endsection --}}
+{{--------------------------------------------------------------------------}}
+
+{{-- @section('writers')
+    @foreach ($comic['writers'] as $writer)
+        <span class="artist">{{ $writer }}</span>,
+    @endforeach
+@endsection --}}
+{{--------------------------------------------------------------------------}}
+
+@section('thumb')
+    {{$comic['image']}}
+@endsection
+{{--------------------------------------------------------------------------}}
+
+@section('sale_date')
+    {{$comic['sale_date']}}
+@endsection
+{{--------------------------------------------------------------------------}}
