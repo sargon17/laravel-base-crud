71cbe84f29afe75f3bba3dc16b8b0b71a5f9689a
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-05-25 17:28:27 +0200

    Create banner.blade.php

---
+++ resources/views/partials/banner.blade.php
@@ -0,0 +1,11 @@
+ <div class="banner">
+    <div class="banner-content">
+        @foreach ( $bannerData as $item )
+        <div class="banner-card">
+        <div class="banner-card__img-container">
+            <img src="{{$item['img']}}" alt="{{$item['alt']}}" />
+        </div>
+        <p>{{$item['title']}}</p>
+      </div>
+        @endforeach
+  </div>
