2c8b6e12b7a8c3dc857ef8173ec78d4e0cedfb69
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-05-26 18:52:31 +0200

    added a default immage

--- database/migrations/2022_05_25_135430_create_comics_table.php
+++ database/migrations/2022_05_25_135430_create_comics_table.php
@@ -17,7 +17,11 @@ class CreateComicsTable extends Migration
             $table->id();
             $table->string("title", 80)->default("Untitled");
             $table->longText("description")->nullable();
-            $table->string("image");
+            $table
+                ->string("image")
+                ->default(
+                    "https://images.unsplash.com/photo-1601645191163-3fc0d5d64e35?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735"
+                );
             $table->decimal("price", 8, 2)->nullable();
             $table->string("series", 80)->nullable();
             $table->date("sale_date")->nullable();
