d10fd356d331f0d84d46b2a05c79c5b77ef51330
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-05-26 18:52:11 +0200

    upd Comic to fillable

--- app/Comic.php
+++ app/Comic.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comic extends Model
 {
-    //
+    protected $fillable = [
+        "title",
+        "description",
+        "image",
+        "price",
+        "series",
+        "sale_date",
+    ];
 }
